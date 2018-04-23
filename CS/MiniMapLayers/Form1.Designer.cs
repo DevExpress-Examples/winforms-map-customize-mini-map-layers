e815ea7d61d1086830c8a3ab0b06c27e6f2b96fb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MiniMapLayers/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MiniMapLayers {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraMap.ImageTilesLayer imageTilesLayer1 = new DevExpress.XtraMap.ImageTilesLayer();
+            DevExpress.XtraMap.BingMapDataProvider bingMapDataProvider1 = new DevExpress.XtraMap.BingMapDataProvider();
+            DevExpress.XtraMap.ColorListLegend colorListLegend1 = new DevExpress.XtraMap.ColorListLegend();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mapControl = new DevExpress.XtraMap.MapControl();
+            this.imageCollection1 = new DevExpress.Utils.ImageCollection(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapControl
+            // 
+            this.mapControl.CenterPoint = new DevExpress.XtraMap.GeoPoint(-33D, 150D);
+            this.mapControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapControl.ImageList = this.imageCollection1;
+            bingMapDataProvider1.BingKey = "Your Bing Key Here";
+            imageTilesLayer1.DataProvider = bingMapDataProvider1;
+            this.mapControl.Layers.Add(imageTilesLayer1);
+            this.mapControl.Legends.Add(colorListLegend1);
+            this.mapControl.Location = new System.Drawing.Point(0, 0);
+            this.mapControl.Name = "mapControl";
+            this.mapControl.Size = new System.Drawing.Size(784, 561);
+            this.mapControl.TabIndex = 0;
+            this.mapControl.ZoomLevel = 6D;
+            // 
+            // imageCollection1
+            // 
+            this.imageCollection1.ImageSize = new System.Drawing.Size(40, 40);
+            this.imageCollection1.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("imageCollection1.ImageStream")));
+            this.imageCollection1.Images.SetKeyName(0, "Ship.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.mapControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraMap.MapControl mapControl;
+        private DevExpress.Utils.ImageCollection imageCollection1;
+    }
+}
+
---
+++ VB/MiniMapLayers/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿Namespace MiniMapLayers
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim imageTilesLayer1 As New DevExpress.XtraMap.ImageTilesLayer()
+            Dim bingMapDataProvider1 As New DevExpress.XtraMap.BingMapDataProvider()
+            Dim colorListLegend1 As New DevExpress.XtraMap.ColorListLegend()
+            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+            Me.mapControl = New DevExpress.XtraMap.MapControl()
+            Me.imageCollection1 = New DevExpress.Utils.ImageCollection(Me.components)
+            DirectCast(Me.mapControl, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.imageCollection1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' mapControl
+            ' 
+            Me.mapControl.CenterPoint = New DevExpress.XtraMap.GeoPoint(-33R, 150R)
+            Me.mapControl.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.mapControl.ImageList = Me.imageCollection1
+            bingMapDataProvider1.BingKey = "Your Bing Key Here"
+            imageTilesLayer1.DataProvider = bingMapDataProvider1
+            Me.mapControl.Layers.Add(imageTilesLayer1)
+            Me.mapControl.Legends.Add(colorListLegend1)
+            Me.mapControl.Location = New System.Drawing.Point(0, 0)
+            Me.mapControl.Name = "mapControl"
+            Me.mapControl.Size = New System.Drawing.Size(784, 561)
+            Me.mapControl.TabIndex = 0
+            Me.mapControl.ZoomLevel = 6R
+            ' 
+            ' imageCollection1
+            ' 
+            Me.imageCollection1.ImageSize = New System.Drawing.Size(40, 40)
+            Me.imageCollection1.ImageStream = (DirectCast(resources.GetObject("imageCollection1.ImageStream"), DevExpress.Utils.ImageCollectionStreamer))
+            Me.imageCollection1.Images.SetKeyName(0, "Ship.png")
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(784, 561)
+            Me.Controls.Add(Me.mapControl)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(Me.mapControl, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.imageCollection1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private mapControl As DevExpress.XtraMap.MapControl
+        Private imageCollection1 As DevExpress.Utils.ImageCollection
+    End Class
+End Namespace
+
